f2dc88e1b8f577522d2b860268134748c2dda841
Kaushik Prajapati <kaushikprajapati1100@gmail.com>
2024-10-03 16:48:50 +0530

    Canged the printing formate, added standard chess indexing for better clarity and also made some modification as required

--- chess.py
+++ chess.py
@@ -9,39 +9,49 @@ class ChessBoard:
         board = [[None for _ in range(8)] for _ in range(8)]  # Create 8x8 grid
         # Place white pawns at row 1 and black pawns at row 6
         for i in range(8):
-            board[1][i] = Pawn('w', 'P')  # White pawn
-            board[6][i] = Pawn('b', 'P')  # Black pawn
+            board[1][i] = Pawn('b', 'P')  # Black pawn
+            board[6][i] = Pawn('w', 'P')  # White pawn
 
         # Define the placement of other pieces (Rooks, Knights, Bishops, Queen, King)
         placement = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
         for i in range(8):
             if placement[i] == 'R':
-                board[0][i] = Rook('w', placement[i])  # White Rook
-                board[7][i] = Rook('b', placement[i])  # Black Rook
+                board[0][i] = Rook('b', placement[i])  # Black Rook
+                board[7][i] = Rook('w', placement[i])  # White Rook
             elif placement[i] == 'N':
-                board[0][i] = Knight('w', placement[i])  # White Knight
-                board[7][i] = Knight('b', placement[i])  # Black Knight
+                board[0][i] = Knight('b', placement[i])  # Black Knight
+                board[7][i] = Knight('w', placement[i])  # White Knight
             elif placement[i] == 'B':
-                board[0][i] = Bishop('w', placement[i])  # White Bishop
-                board[7][i] = Bishop('b', placement[i])  # Black Bishop
+                board[0][i] = Bishop('b', placement[i])  # Black Bishop
+                board[7][i] = Bishop('w', placement[i])  # White Bishop
             elif placement[i] == 'Q':
-                board[0][i] = Queen('w', placement[i])   # White Queen
-                board[7][i] = Queen('b', placement[i])   # Black Queen
+                board[0][i] = Queen('b', placement[i])   # Black Queen
+                board[7][i] = Queen('w', placement[i])   # White Queen
             elif placement[i] == 'K':
-                board[0][i] = King('w', placement[i])    # White King
-                board[7][i] = King('b', placement[i])    # Black King
+                board[0][i] = King('b', placement[i])    # Black King
+                board[7][i] = King('w', placement[i])    # White King
         return board
 
     # Function to print the board in a human-readable format
     def print_board(self):
+
+        i=8
+        print("     ----------------- ")
         for row in self.board:
-            print(' '.join([str(piece) if piece else '.' for piece in row]))  # Display pieces or '.' for empty squares
+            print(f'  {i} |', ' '.join([str(piece) if piece else '.' for piece in row]), '|')  # Display pieces or '.' for empty squares
+            i-=1
+        print("     ----------------- ")
+        print("      A B C D E F G H")  # Display pieces or '.' for empty squares
 
     # Placeholder function for moving pieces (to be implemented)
     def move_piece(self, start, end):
         
         start_x, start_y = start
         end_x, end_y = end
+
+        # To make rook or king touched
+        if self.board[start_x][start_y].__repr__() in ('k', 'r', 'K', 'R'):
+            self.board[start_x][start_y].is_untouched = False
 
         self.board[end_x][end_y] = self.board[start_x][start_y]
         self.board[start_x][start_y] = None
@@ -61,15 +71,17 @@ class ChessBoard:
             return False
         
         # Check for valid piece color
+        print(self.board[start_x][start_y].color, player_color)
         if self.board[start_x][start_y] and (self.board[start_x][start_y].color != player_color[0]):
             return False
         
         # Check for castle move before
-        if self.board[start_x][start_y].repr().lower() == 'k': 
+        if self.board[start_x][start_y].__repr__() in ('k', 'K'): 
             is_castle_move = self.board[start_x][start_y].is_valid_king_castle_move(self.board, start, end)
-            self.perform_rook_move_for_king_castle(start, end)
-            return True
-
+            if is_castle_move:
+                self.perform_rook_move_for_king_castle(start, end)
+                return True
+        
         return self.board[start_x][start_y].is_valid_move(board=self.board, start=start, end=end)
 
     # Function to perform rook move if the move is king castle
@@ -82,25 +94,25 @@ class ChessBoard:
         rook_end_x, rook_end_y = 0, 0
         # Four possible castles
 
-        if start_x == 0 and start_y == 3 and end_x == 0 and end_y == 1:
+        if start_x == 7 and start_y == 4 and end_x == 7 and end_y == 6:
             # White-King Kingside Castle
-            rook_start_x, rook_start_y = (0, 0)
-            rook_end_x, rook_end_y = (0, 2)
-
-        elif start_x == 0 and start_y == 3 and end_x == 0 and end_y == 5:
-            # White-King Queenside Castle
-            rook_start_x, rook_start_y = (0, 7)
-            rook_end_x, rook_end_y = (0, 4)
-    
-        elif start_x == 7 and start_y == 3 and end_x == 7 and end_y == 1:
-            # Black-King Kingside Castle
-            rook_start_x, rook_start_y = (7, 0)
-            rook_end_x, rook_end_y = (7, 2)
-        
-        elif start_x == 7 and start_y == 3 and end_x == 7 and end_y == 5:
-            # Black-King Queenside Castle
             rook_start_x, rook_start_y = (7, 7)
-            rook_end_x, rook_end_y = (7, 4)
+            rook_end_x, rook_end_y = (7, 5)
+
+        elif start_x == 7 and start_y == 4 and end_x == 7 and end_y == 2:
+            # White-King Queenside Castle
+            rook_start_x, rook_start_y = (7, 0)
+            rook_end_x, rook_end_y = (7, 3)
+    
+        elif start_x == 0 and start_y == 4 and end_x == 0 and end_y == 6:
+            # Black-King Kingside Castle
+            rook_start_x, rook_start_y = (0, 7)
+            rook_end_x, rook_end_y = (0, 5)
+        
+        elif start_x == 0 and start_y == 4 and end_x == 0 and end_y == 2:
+            # Black-King Queenside Castle
+            rook_start_x, rook_start_y = (0, 0)
+            rook_end_x, rook_end_y = (0, 3)
         else:
             return
         
@@ -143,18 +155,18 @@ class Pawn:
         start_x, start_y = start
         end_x, end_y = end
 
-        # Black Pawn Forward Move: (i-1, j) or (i-2, j)
-        # White Pawn Forward Move: (i+1, j) or (i+2, j)
+        # White Pawn Forward Move: (i-1, j) or (i-2, j)
+        # Black Pawn Forward Move: (i+1, j) or (i+2, j)
 
-        del_black = [(-1, 0), (-2, 0)]
-        del_white = [(1, 0), (2, 0)]
+        del_white = [(-1, 0), (-2, 0)]
+        del_black = [(1, 0), (2, 0)]
 
         is_valid_end_position = False
 
         # Check for valid position
         if self.color == 'w':
             # For first move we have two choices
-            if start_x == 1:
+            if start_x == 6:
                 for dx, dy in del_white:
                     is_valid_end_position = is_valid_end_position or (start_x + dx == end_x and start_y + dy == end_y)
             else:
@@ -164,7 +176,7 @@ class Pawn:
         else:
 
             # For first move we have two choices
-            if start_x == 6:
+            if start_x == 1:
                 for dx, dy in del_black:
                     is_valid_end_position = is_valid_end_position or (start_x + dx == end_x and start_y + dy == end_y)
             else:
@@ -183,11 +195,11 @@ class Pawn:
         start_x, start_y = start
         end_x, end_y = end
 
-        # Black Pawn Diagonal Move: (i-1, j-1) or (i-1, j+1)
-        # White Pawn Diagonal Move: (i+1, j-1) or (i+1, j+1)
+        # White Pawn Diagonal Move: (i-1, j-1) or (i-1, j+1)
+        # Black Pawn Diagonal Move: (i+1, j-1) or (i+1, j+1)
 
-        del_black = [(-1, -1), (-1, 1)]
-        del_white = [(1, -1), (1, 1)]
+        del_white = [(-1, -1), (-1, 1)]
+        del_black = [(1, -1), (1, 1)]
 
         is_valid_end_position = False
 
@@ -434,54 +446,54 @@ class King:
         no_piece_in_bw = True
         is_untouched = False
 
-        if start_x == 0 and start_y == 3 and end_x == 0 and end_y == 1:
+        if start_x == 7 and start_y == 4 and end_x == 7 and end_y == 6:
             # White-King Kingside Castle
-            # King's Position should be (0,3) and Rook's Position should be (0,0) and there should not be any piece between them
-            # Also the end position should be (0, 1)
+            # King's Position should be (7,4) and Rook's Position should be (7,7) and there should not be any piece between them
+            # Also the end position should be (7, 6)
             
-            rook_is_present = (board[0][0].repr().lower() == 'r')
+            rook_is_present = (board[7][7].__repr__() in ('r', 'R'))
             no_piece_in_bw = True
-            for k in range(1, 3):
-                no_piece_in_bw = no_piece_in_bw and (not board[0][k])
-            is_untouched = self.is_untouched or (rook_is_present and board[0][0].is_untouched)
-
-        elif start_x == 0 and start_y == 3 and end_x == 0 and end_y == 5:
-            # White-King Queenside Castle
-            # King's Position should be (0,3) and Rook's Position should be (0,7) and there should not be any piece between them
-            # Also the end position should be (0,5)
-
-            rook_is_present = (board[0][7].repr().lower() == 'r')
-            no_piece_in_bw = True
-            for k in range(4, 7):
-                no_piece_in_bw = no_piece_in_bw and (not board[0][k])
-            is_untouched = self.is_untouched or (rook_is_present and board[0][7].is_untouched)
-
-        
-        elif start_x == 7 and start_y == 3 and end_x == 7 and end_y == 1:
-            # Black-King Kingside Castle
-            # King's Position should be (7,3) and Rook's Position should be (7, 0) and there should not be any piece between them
-            # Also the end position should be (7,1)
-            
-            rook_is_present = (board[7][0].repr().lower() == 'r')
-            no_piece_in_bw = True
-            for k in range(1, 3):
-                no_piece_in_bw = no_piece_in_bw and (not board[7][k])
-            is_untouched = self.is_untouched or (rook_is_present and board[7][0].is_untouched)
-
-        
-        elif start_x == 7 and start_y == 3 and end_x == 7 and end_y == 5:
-            # Black-King Queenside Castle
-            # King's Position should be (7,3) and Rook's Position should be (7, 7) and there should not be any piece between them
-            # Also the end position should be (7, 5)
-
-            rook_is_present = (board[7][7].repr().lower() == 'r')
-            no_piece_in_bw = True
-            for k in range(4, 7):
+            for k in range(5, 7):
                 no_piece_in_bw = no_piece_in_bw and (not board[7][k])
             is_untouched = self.is_untouched or (rook_is_present and board[7][7].is_untouched)
 
+        elif start_x == 7 and start_y == 4 and end_x == 7 and end_y == 2:
+            # White-King Queenside Castle
+            # King's Position should be (7,4) and Rook's Position should be (7,0) and there should not be any piece between them
+            # Also the end position should be (7,2)
+
+            rook_is_present = (board[7][0].__repr__() in ('r', 'R'))
+            no_piece_in_bw = True
+            for k in range(1, 4):
+                no_piece_in_bw = no_piece_in_bw and (not board[7][k])
+            is_untouched = self.is_untouched or (rook_is_present and board[7][0].is_untouched)
+
+        elif start_x == 0 and start_y == 4 and end_x == 0 and end_y == 6:
+            # Black-King Kingside Castle
+            # King's Position should be (0,4) and Rook's Position should be (0,7) and there should not be any piece between them
+            # Also the end position should be (7,1)
+            
+            rook_is_present = (board[0][7].__repr__() in ('r', 'R'))
+            no_piece_in_bw = True
+            for k in range(5, 7):
+                no_piece_in_bw = no_piece_in_bw and (not board[0][k])
+            is_untouched = self.is_untouched or (rook_is_present and board[0][7].is_untouched)
+
+        elif start_x == 0 and start_y == 4 and end_x == 0 and end_y == 2:
+            # Black-King Queenside Castle
+            # King's Position should be (0,4) and Rook's Position should be (0,0) and there should not be any piece between them
+            # Also the end position should be (0,1)
+
+            rook_is_present = (board[0][0].__repr__() in ('r', 'R'))
+            no_piece_in_bw = True
+            for k in range(1, 4):
+                no_piece_in_bw = no_piece_in_bw and (not board[0][k])
+            is_untouched = self.is_untouched or (rook_is_present and board[0][0].is_untouched)
+
         else:
             return False
+        
+        return rook_is_present and no_piece_in_bw and is_untouched
 
     # Placeholder for vertical move validation
     def is_vertical_move(self, start, end):
@@ -602,8 +614,7 @@ class Queen:
         # Check for valid diagonal move usign the condition: (x, y) where |x-y| = |i-j|
         is_dia_move = (abs(start_x-start_y) == abs(end_x-end_y))
         return is_dia_move
-        
-
+    
 # Class representing the game logic
 class Game:
     def __init__(self):
@@ -636,40 +647,36 @@ class Game:
             print(f"{self.turn.capitalize()}'s Turn")
 
             # Prompt for starting coordinate
-            start_input = input("Enter starting coordinate (x,y) or 'exit' to quit: ").strip()
+            start_input = input("Enter starting location of piece or 'exit' to quit: ").strip()
             if start_input.lower() == 'exit':
                 print("Exiting the game.")
                 break
 
             # Prompt for ending coordinate
-            end_input = input("Enter ending coordinate (x,y): ").strip()
+            end_input = input("Enter ending location of piece (x,y): ").strip()
             if end_input.lower() == 'exit':
-                print("Exiting the game.")
-                break
-
-            # Prompt for piece
-            piece_input = input("Enter piece (P, R, N, B, Q, K): ").strip()
-            if piece_input.lower() == 'exit':
                 print("Exiting the game.")
                 break
 
             # Process the inputs
             try:
-                start_x, start_y = map(int, start_input.split(','))
-                end_x, end_y = map(int, end_input.split(','))
 
-                print(f"Move requested: Piece at ({start_x}, {start_y}) to ({end_x}, {end_y}) as {piece_input}")
+                # Convert string input location to grid coordinates
+                start_x, start_y = map(int, [8-int(start_input[1]), ord(start_input[0])-ord('A')])
+                end_x, end_y = map(int, [8-int(end_input[1]), ord(end_input[0])-ord('A')])
+
+                print(f"Move requested: Piece at {start_input}({start_x}, {start_y}) to {end_input}({end_x}, {end_y}).")
 
             except ValueError:
                 print("Invalid input format. Please ensure you're using (x,y) format.")
                 continue
             
-            # print(start_x, start_y, end_x, end_y)
             is_valid_move = self.board.is_valid_move(start=(start_x, start_y), end=(end_x, end_y), player_color=self.turn)
             if not is_valid_move:
                 print("Invalid Move! Try again.")
                 continue
             
+            # Move piece if move is valid
             self.board.move_piece(start=(start_x, start_y), end=(end_x, end_y))
 
             # Switch the turn after capturing the move

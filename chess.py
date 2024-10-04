a4085e30900e2c6a80021497e7937f6ad00631db
Kaushik Prajapati <kaushikprajapati1100@gmail.com>
2024-10-04 08:54:24 +0530

    Implemented is_pawn_promotion_possible() and ask_for_pawn_promotion() methods and improved diagonal move logic for bishop and queen

--- chess.py
+++ chess.py
@@ -71,7 +71,6 @@ class ChessBoard:
             return False
         
         # Check for valid piece color
-        print(self.board[start_x][start_y].color, player_color)
         if self.board[start_x][start_y] and (self.board[start_x][start_y].color != player_color[0]):
             return False
         
@@ -121,11 +120,51 @@ class ChessBoard:
     
     # Placeholder function to check if pawn promotion is possible
     def is_pawn_promotion_possible(self, start, end, player_color):
-        pass
+        
+        start_x, start_y = start
+        end_x, end_y = end
 
+        # The moved piece has to be pawn
+        is_pawn = self.board[start_x][start_y].__repr__() in ('p', 'P')
+        # is_pawn = isinstance(self.board[start_x][start_y], Pawn)
+
+        # The ending row should be '0' if the player is 'white' and '7' if the player is 'black'
+        reached_end = (player_color == 'white' and  end_x == 0) or (player_color == 'black' and end_x == 7)
+        
+        # Both the above condition has to be satisfied
+        return is_pawn and reached_end
+    
     # Placeholder function for asking player to promote pawn
     def ask_for_pawn_promotion(self, start, end, player_color):
-        pass
+
+        start_x, start_y = start
+        end_x, end_y = end
+
+        promotion_choices = {
+            '1': Queen(player_color[0], 'Q'),
+            '2': Rook(player_color[0], 'R'),
+            '3': Bishop(player_color[0], 'B'),
+            '4': Knight(player_color[0], 'N')
+        }
+        
+        # Display Options
+        print("Your pawn can be promoted! Please choose a piece for promotion: \n1. Queen \n2. Rook \n3. Bishop \n4. Knight")
+        
+        while True:
+            piece = input("Enter the number corresponding to the piece you want (1-4): ")
+            if piece not in promotion_choices:
+                print("Invalid input. Please choose a number between 1 and 4.")
+            else:
+                break
+        
+        # First move the pawn to end location
+        self.move_piece(start, end)
+
+        # Promote the pawn
+        self.board[end_x][end_y] = promotion_choices[piece]
+
+        if piece == '2':
+            self.board[end_x][end_y].is_untouched = False
 
 # Class representing a pawn piece
 class Pawn:
@@ -380,8 +419,8 @@ class Bishop:
         start_x, start_y = start
         end_x, end_y = end
 
-        # Check for valid diagonal move usign the condition: (x, y) where |x-y| = |i-j|
-        is_dia_move = (abs(start_x-start_y) == abs(end_x-end_y))
+        # Check for valid diagonal move usign the condition: (x, y) where |x-i| = |y-j|
+        is_dia_move = (abs(start_x - end_x) == abs(start_y - end_y))
         return is_dia_move
 
 # Class representing a king piece
@@ -471,7 +510,7 @@ class King:
         elif start_x == 0 and start_y == 4 and end_x == 0 and end_y == 6:
             # Black-King Kingside Castle
             # King's Position should be (0,4) and Rook's Position should be (0,7) and there should not be any piece between them
-            # Also the end position should be (7,1)
+            # Also the end position should be (0, 6)
             
             rook_is_present = (board[0][7].__repr__() in ('r', 'R'))
             no_piece_in_bw = True
@@ -482,7 +521,7 @@ class King:
         elif start_x == 0 and start_y == 4 and end_x == 0 and end_y == 2:
             # Black-King Queenside Castle
             # King's Position should be (0,4) and Rook's Position should be (0,0) and there should not be any piece between them
-            # Also the end position should be (0,1)
+            # Also the end position should be (0, 2)
 
             rook_is_present = (board[0][0].__repr__() in ('r', 'R'))
             no_piece_in_bw = True
@@ -611,8 +650,8 @@ class Queen:
         start_x, start_y = start
         end_x, end_y = end
 
-        # Check for valid diagonal move usign the condition: (x, y) where |x-y| = |i-j|
-        is_dia_move = (abs(start_x-start_y) == abs(end_x-end_y))
+        # Check for valid diagonal move usign the condition: (x, y) where |x-i| = |y-j|
+        is_dia_move = (abs(start_x - end_x) == abs(start_y - end_y))
         return is_dia_move
     
 # Class representing the game logic
@@ -653,22 +692,26 @@ class Game:
                 break
 
             # Prompt for ending coordinate
-            end_input = input("Enter ending location of piece (x,y): ").strip()
+            end_input = input("Enter ending location of piece: ").strip()
             if end_input.lower() == 'exit':
                 print("Exiting the game.")
                 break
+            
+            if len(start_input) != 2 or len(end_input) != 2:
+                print("Invalid input format. Please ensure you're using standard format.")
+                continue
 
             # Process the inputs
             try:
 
                 # Convert string input location to grid coordinates
-                start_x, start_y = map(int, [8-int(start_input[1]), ord(start_input[0])-ord('A')])
-                end_x, end_y = map(int, [8-int(end_input[1]), ord(end_input[0])-ord('A')])
+                start_x, start_y = 8-int(start_input[1]), ord(start_input[0].upper()) - ord('A')
+                end_x, end_y = 8-int(end_input[1]), ord(end_input[0]).upper() - ord('A')
 
                 print(f"Move requested: Piece at {start_input}({start_x}, {start_y}) to {end_input}({end_x}, {end_y}).")
 
             except ValueError:
-                print("Invalid input format. Please ensure you're using (x,y) format.")
+                print("Invalid input format. Please ensure you're using standard format.")
                 continue
             
             is_valid_move = self.board.is_valid_move(start=(start_x, start_y), end=(end_x, end_y), player_color=self.turn)
@@ -676,8 +719,11 @@ class Game:
                 print("Invalid Move! Try again.")
                 continue
             
-            # Move piece if move is valid
-            self.board.move_piece(start=(start_x, start_y), end=(end_x, end_y))
+            if self.board.is_pawn_promotion_possible(start=(start_x, start_y), end=(end_x, end_y), player_color=self.turn):
+                self.board.ask_for_pawn_promotion(start=(start_x, start_y), end=(end_x, end_y), player_color=self.turn)
+            else:
+                # Move piece if move is valid
+                self.board.move_piece(start=(start_x, start_y), end=(end_x, end_y))
 
             # Switch the turn after capturing the move
             self.switch_turn()
